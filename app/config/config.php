34181e61028f295c58b5e42bef6a1a6ac57fc440
Carlos Vieira <carlepsvieira@gmail.com>
2023-04-17 12:45:32 -0300

    config set and running

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,15 @@
 <?php
 // Database Params
 define('DB_HOST', 'localhost');
-define('DB_USER', '_YOUR_USER_');
-define('DB_PASS', '_YOUR_PASSWORD_');
-define('DB_NAME', '_YOUR_DATABASE_NAME_');
+define('DB_USER', 'root');
+define('DB_PASS', 'scandiweb');
+define('DB_NAME', 'scandi_db');
 
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 
 // URL Root
-define('URLROOT', '/_YOUR_URL_'); // development format example "/yoururlroot"
+define('URLROOT', '/scandimanager'); // development format example "/yoururlroot"
 
 // Site Name
-define('SITENAME', "_YOUR_SITE_NAME_");
+define('SITENAME', "Scandi Manager");
